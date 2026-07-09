0459ea1a27f740e64f741d8e002210bd3969820b
JTSanthosh0007 <djshettydjshetty@gmail.com>
2026-07-09 22:52:33 +0530

    fix: include new artwork widget, proguard rules, and fix animate import

--- lib/features/albums/widgets/album_card.dart
+++ lib/features/albums/widgets/album_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import '../../../core/theme/app_colors.dart';
 import '../../../core/models/album.dart';
 import '../../../shared/widgets/cached_artwork_image.dart';
